4dbd560bf4ba57854913486cb3d76b25117f7857
DominikWojdala <wojdaladominik@gmail.com>
2024-01-14 14:37:35 +0100

    3 test

--- main.py
+++ main.py
@@ -1,5 +1,47 @@
 import cv2
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class PeopleCounterStatic(Resource):
+    def get(self):
+        # load image
+        image = cv2.imread('wroclaw_glowny.jpeg')
+        image = cv2.resize(image, (700, 400))
+
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+        return {'peopleCount': len(rects)}
+
+
+class PeopleCounterDynamicUrl(Resource):
+    def get(self):
+        # TODO:
+        # 1. Pobrać zdjęcie z otrzymanego adresu
+        # 2. Pobrane zdjęcie można zapisać na dysku lub przetwarzać je w pamięci podręcznej
+        # 3. Załadowane zjęcie do zmiennej image przekazać do algorytmu hog.detectMultiScale i zwrócić z endpointu liczbę wykrytych osób.
+
+        url = request.args.get('url')
+        print('url', url)
+        return {'peopleCount': 0}
+
+
+api.add_resource(PeopleCounterStatic, '/')
+api.add_resource(PeopleCounterDynamicUrl, '/dynamic')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''import cv2
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 # initialize the HOG descriptor/person detector
@@ -12,10 +54,11 @@ api = Api(app)
 
 class PeopleCounter(Resource):
     def get(self):
+        url = request.args.get('url:')
+        print('url:', url)
         # load image
-        image = cv2.imread('Zakupy-przedswiateczne-w-PRL.jpg')
+        image = cv2.imread('')
         image = cv2.resize(image, (700, 400))
-
         # detect people in the image
         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
@@ -26,7 +69,7 @@ api.add_resource(PeopleCounter, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+'''
 '''import cv2
 from flask import Flask
 from flask_restful import Resource, Api
